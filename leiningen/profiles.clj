e2e52b0851ee4ee7134c6af6b30c5c6b6af6a465
Giovanni Martina <gio@popado.co>
2018-06-24 11:37:56 -0300

    Add leiningen user profile

---
+++ leiningen/profiles.clj
@@ -0,0 +1,5 @@
+{:user {:plugins [[cider/cider-nrepl "0.16.0"]
+                  [venantius/ultra "0.5.2"]
+                  [lein-midje "3.2.1"]
+                  [lein-kibit "0.1.6"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.13"]]}}
