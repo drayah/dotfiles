f38803fd11b29eb7c61f483c7e46afe2911ac0e2
Giovanni Martina <gio@popado.co>
2018-10-10 12:40:42 -0300

    Remove nrepl from leiningen profiles

--- leiningen/profiles.clj
+++ leiningen/profiles.clj
@@ -1,8 +1,6 @@
-{:user {:plugins [[cider/cider-nrepl "0.16.0"]
-                  [venantius/ultra "0.5.2"]
+{:user {:plugins [[venantius/ultra "0.5.2"]
                   [lein-midje "3.2.1"]
                   [lein-kibit "0.1.6"]
                   [jonase/eastwood "0.2.9"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.13"]
-                       [jonase/kibit "0.1.6" :exclusions [org.clojure/clojure]]
+        :dependencies [[jonase/kibit "0.1.6" :exclusions [org.clojure/clojure]]
                        [jonase/eastwood "0.2.9" :exclusions [org.clojure/clojure]]]}}
