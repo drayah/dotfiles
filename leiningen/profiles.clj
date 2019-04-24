f6b69e29f0297bdc838953db54270a7bb54b29d9
Giovanni Martina <drayah@gmail.com>
2019-04-24 15:44:17 -0300

    Remove lein profiles

--- leiningen/profiles.clj
+++
@@ -1,6 +0,0 @@
-{:user {:plugins [[venantius/ultra "0.5.2"]
-                  [lein-midje "3.2.1"]
-                  [lein-kibit "0.1.6"]
-                  [jonase/eastwood "0.2.9"]]
-        :dependencies [[jonase/kibit "0.1.6" :exclusions [org.clojure/clojure]]
-                       [jonase/eastwood "0.2.9" :exclusions [org.clojure/clojure]]]}}
