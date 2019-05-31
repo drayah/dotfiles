6119b39f74ba516f6e9cad6bed104688af655464
Giovanni Martina <drayah@gmail.com>
2019-05-31 08:43:25 -0300

    Update doom init

--- doom-emacs/init.el
+++ doom-emacs/init.el
@@ -129,7 +129,9 @@
         +babel           ; running code in org
         +capture         ; org-capture in and outside of Emacs
         +export          ; Exporting org to whatever you want
-        +present)        ; Emacs for presentations
+        +habit           ; Keep track of your habits
+        +present         ; Emacs for presentations
+        +protocol)       ; Support for org-protocol:// links
        ;;perl              ; write code no one else can comprehend
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
