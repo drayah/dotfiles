fd88c5c73c74d506955141564738aeb429942f96
Giovanni Martina <drayah@gmail.com>
2019-09-05 17:35:34 -0300

    Set default plantuml exec mode to jar file

--- doom-emacs/config.el
+++ doom-emacs/config.el
@@ -88,3 +88,7 @@
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2))
+
+;; plantuml
+(after! plantuml-mode
+  (setq plantuml-default-exec-mode 'jar))
